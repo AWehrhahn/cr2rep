44f3a00703358921662392679d13eae7a285e113
Thomas Marquart <thomas@marquart.se>
2017-02-10 11:33:37 +0100

    add cr2res/cr2res_slitdec.h

---
+++ cr2res/cr2res_slitdec.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the CR2RES Pipeline
+ * Copyright (C) 2002,2003 European Southern Observatory
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02111-1307  USA
+ */
+
+#ifndef CR2RES_SLITDEC_H
+#define CR2RES_SLITDEC_H
+
+/*-----------------------------------------------------------------------------
+                                   Includes
+ -----------------------------------------------------------------------------*/
+
+#include <cpl.h>
+#include "cr2res_utils.h"
+
+/*-----------------------------------------------------------------------------
+                                       Prototypes
+ -----------------------------------------------------------------------------*/
+
+cpl_image * cr2res_slitdec_vert(  cpl_image * img_in, // full detector image
+                         cpl_vector * ycen, // current order mid-line y-coordinates
+                         int height, // number of pix above and below mid-line
+                         int swath, // width per swath
+                         int oversample, // factor for oversampling
+                         cpl_vector * slit_func, // slit illumination
+                         cpl_vector * spec, // spectrum
+     ) ;
+
+#endif
